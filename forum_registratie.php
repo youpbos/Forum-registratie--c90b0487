9509277c6935141a9a850faff13e86ac19f3a458
youp bos <2107418@talnet.nl>
2020-03-02 10:19:43 +0100

    Uitwerking van de exercise

---
+++ forum_registratie.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Forum registratie</h1>
+    <form action="registratie_handler.php" method="POST">
+        <input type="text" name="username" id="" placeholder="Name">
+        <span>Username</span>
+        <div>
+            <h1></h1>
+        </div>
+        <input type="email" name="useremail" id="" placeholder="Email">
+        <span>Email</span>
+        <div>
+            <h1></h1>
+        </div>
+        <input type="number" name="age" id="" placeholder="Age">
+        <span>Age</span>
+        <div>
+            <h1></h1>
+        </div>
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+</html>
---
+++ registratie_handler.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Form registratie</h1>";
+echo "Jouw username is " . $_POST["username"];
+echo "<h1></h1>";
+echo "Jouw E-mail is " . $_POST["useremail"];
+echo "<h1></h1>";
+echo "Jouw age is " . $_POST["age"];
